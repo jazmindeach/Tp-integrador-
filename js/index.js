41872ff05721da09af9b1961f839428f3e79abf7
jazmindeach <jdeachaval@udesa.edu.ar>
2021-11-23 19:19:11 -0300

    Nose

--- js/index.js
+++ js/index.js
@@ -2,77 +2,78 @@ window.addEventListener("load",function(){    //Controlar que todo el HTML este 
 
 
 
-fetch("https://api.themoviedb.org/3/movie/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1") //genera una promesa. Trae la infromacion de la API de peliculas con fetch
-    .then(function(respuesta){ //espero que me traiga la infromación para hacer lo que wuiero
-        return respuesta.json()   //Convierte los datos a formato JSON
-    })
-    .then(function(datos){ //
-        console.log(datos); //imprimo los datos 
-        console.log(datos.results[0]);   // Obtenemos los datos que necesitamos imprimo los resutados
-
-        //Capturamos el elemento del DOM que queremos modificar
-        let peliculasPopulares = document.getElementById ("1") // Para que se visualice 
-       datos.results.forEach(data => {
-            peliculasPopulares.innerHTML += `<article class="cajahija"><a href="detail-movie.html?id=${data.id}"> <img class="" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"></a>
-            <div> 
-            <h3 class="titulos"> <a href="detail-movie.html?id=${data.id}"> ${data.title} </a> </h3>
-            
-            </div>
-            </article>`
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1") //genera una promesa. Trae la infromacion de la API de peliculas con fetch
+        .then(function(respuesta){ //espero que me traiga la infromación para hacer lo que wuiero
+            return respuesta.json()   //Convierte los datos a formato JSON
         })
-
-    })
-    .catch(function(error){
-        console.log(error);
-    })
-
-
-fetch("https://api.themoviedb.org/3/tv/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1") //Buscamos infromación de la API
-    .then(function(respuesta){
-        return respuesta.json()
-    })
-    .then(function(datos){
-        console.log(datos);
-        console.log(datos.results[0]);
-        let seriesPopulares = document.getElementById("2")
-        datos.results.forEach(data => {
-            seriesPopulares.innerHTML += `<artcile class="cajahija"><a href="detail-series.html?id=${data.id}"> <img class="" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"></a>
-            <div> 
-            <h3 class="titulos"> <a href="detail-movie.html"> ${data.name} </a> </h3>
-            
-            </div>
-            </article>`
+        .then(function(datos){ //
+            console.log(datos); //imprimo los datos 
+            console.log(datos.results[0]);   // Obtenemos los datos que necesitamos imprimo los resutados
+    
+            //Capturamos el elemento del DOM que queremos modificar
+            let peliculasPopulares = document.getElementById ("1") // Para que se visualice 
+           datos.results.forEach(data => {
+                peliculasPopulares.innerHTML += `<article class="cajahija"><a href="detail-movie.html?id=${data.id}"> <img class="" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"></a>
+                <div> 
+                <h3 class="titulos"> <a href="detail-movie.html?id=${data.id}"> ${data.title} </a> </h3>
+                
+                </div>
+                </article>`
+            })
+    
         })
-
-    })
-    .catch(function(error){
-        console.log(error);
-    })
-
-
-
-fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1") //Buscamos infromación de la API
-    .then(function(respuesta){
-        return respuesta.json() //Convierte los datos a fromato json
-    })
-    .then(function(datos){
-        console.log(datos);
-        console.log(datos.results[0]); //Obtenemos los datos que necesitamos 
-
-        //Capturamos el elemento del DOM que queremos modificar
-        let peliculasBuenas = document.getElementById("3")
-        datos.results.forEach(data => {
-            peliculasBuenas.innerHTML += `<artcile class="cajahija"><a href="detail-movie.html?id=${data.id}"> <img class="" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"></a>
-            <div> 
-            <h3 class="titulos"> <a href="detail-movie.html"> ${data.title} </a> </h3>
-       
-            </div>
-            </article>`
+        .catch(function(error){
+            console.log(error);
         })
-
+    
+    
+    fetch("https://api.themoviedb.org/3/tv/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1") //Buscamos infromación de la API
+        .then(function(respuesta){
+            return respuesta.json()
+        })
+        .then(function(datos){
+            console.log(datos);
+            console.log(datos.results[0]);
+            let seriesPopulares = document.getElementById("2")
+            datos.results.forEach(data => {
+                seriesPopulares.innerHTML += `<artcile class="cajahija"><a href="detail-series.html?id=${data.id}"> <img class="" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"></a>
+                <div> 
+                <h3 class="titulos"> <a href="detail-movie.html"> ${data.name} </a> </h3>
+                
+                </div>
+                </article>`
+            })
+    
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    
+    
+    
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1") //Buscamos infromación de la API
+        .then(function(respuesta){
+            return respuesta.json() //Convierte los datos a fromato json
+        })
+        .then(function(datos){
+            console.log(datos);
+            console.log(datos.results[0]); //Obtenemos los datos que necesitamos 
+    
+            //Capturamos el elemento del DOM que queremos modificar
+            let peliculasBuenas = document.getElementById("3")
+            datos.results.forEach(data => {
+                peliculasBuenas.innerHTML += `<artcile class="cajahija"><a href="detail-movie.html?id=${data.id}"> <img class="" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"></a>
+                <div> 
+                <h3 class="titulos"> <a href="detail-movie.html"> ${data.title} </a> </h3>
+           
+                </div>
+                </article>`
+            })
+    
+        })
+        .catch(function(error){
+            console.log(error);
+        })
     })
-    .catch(function(error){
-        console.log(error);
-    })
-})
 
+    
