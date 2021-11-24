f45c26396e86809ee532f5837ca8aeb854c36bde
luciadenham <ldenham@udesa.edu.ar>
2021-11-23 22:25:40 -0300

    ultimos detalles lucia

--- js/generos.js
+++ js/generos.js
@@ -1,20 +1,20 @@
 window.addEventListener("load",function(){
 
-    let generospelicula = document.getElementById("1")
-    let generoserie = document.getElementById("2")
+    let generospelicula = document.getElementById("titulogenerospel") // poner palabra
+    let generoserie = document.getElementById("titulogenerosserie")
 
     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US`)
     .then(function(respuesta){
-        return respuesta.json()   //Convierte los datos a formato JSON
+        return respuesta.json()  
     })
     .then(function(datos){
         console.log(datos);
-        datos.genres.forEach(data => { 
-            generospelicula.innerHTML+= `<li><a href="./detail-genres.html?id=${data.id}&name=${data.name}&categoria=pelicula"> ${data.name} </a> </li>`
-            
-        });
-    
+        for(let i = 0; i < datos.genres.length; i++){
 
+                generospelicula.innerHTML+= `<li><a href="./detail-genres.html?id=${datos.genres[i].id}&name=${datos.genres[i].name}&categoria=pelicula"> ${datos.genres[i].name} </a> </li>`
+                
+
+            }
     
 
     })
@@ -24,17 +24,17 @@ window.addEventListener("load",function(){
 
     fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US`)
     .then(function(respuesta){
-        return respuesta.json()   //Convierte los datos a formato JSON
+        return respuesta.json() 
     })
-    .then(function(datos){
+    .then (function (datos){
         console.log(datos);
-        datos.genres.forEach(data => { 
-            generoserie.innerHTML+= `<li><a href="./detail-genres.html?id=${data.id}&name=${data.name}&categoria=serie"> ${data.name} </a> </li>`
-            
-        });
+        for(let i = 0; i < datos.genres.length; i++){
+                generoserie.innerHTML+= `<li><a href="./detail-genres.html?id=${datos.genres[i].id}&name=${datos.genres[i].name}&categoria=pelicula"> ${datos.genres[i].name} </a> </li>`
+
+
+            }
     
 
-    
 
     })
     .catch(function(error){
